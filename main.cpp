b391b1ccc6c22bcd38f0af5ded56cf9193bdc0c6
Johan Paredes <jmpareds@gmail.com>
2023-09-29 18:57:27 -0400

    added north_west_method for initial solution

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 vector<int> getVector(int m, string name = "", string name2 = ""){
     vector<int> v(m, 0);
-
+    cout << '\n';
     for(int i = 0; i < m; i++){
         cout << "Enter " << name << " value for " << name2 << i+1 << ": ";
         cin >> v[i];
@@ -18,10 +18,38 @@ vector<vector<int>> getMatrix(int n, int m, string name = ""){
     vector<vector<int>> data(n);
 
     for(int i = 0; i < n; i++){
-        data[i] = getVector(m, name, to_string(i+1)+"-");
+        data[i] = getVector(m, name, "cell " + to_string(i+1) + "-");
     }
 
     return data;
+}
+
+vector<vector<int>> north_west_method(vector<vector<int>> cost_matrix, vector<int> supply, vector<int> demand){
+    int n = cost_matrix.size(), m = cost_matrix[0].size();
+    vector<vector<int>> res(n, vector<int>(m, 0));
+
+    int i = 0, j = 0;
+
+    while( supply[n - 1] != 0 && demand[m - 1] != 0 ){
+        if(supply[i] < demand[j]){
+            res[i][j] = supply[i];
+            demand[j] -= supply[i]; 
+            supply[i] = 0;
+        } else if (supply[i] > demand[j]) {
+            res[i][j] = demand[j];
+            supply[i] -= demand[j]; 
+            demand[j] = 0;
+        } else {
+            res[i][j] = supply[i];
+            supply[i] = 0;
+            demand[j] = 0;   
+        }
+
+        if(supply[i]==0) i++;
+        if(demand[j]==0) j++;
+    }
+
+    return res;
 }
 
 int main (){
@@ -31,9 +59,11 @@ int main (){
     cout << "How many destinations will the problem have: ";
     cin >> m;
 
-    vector<vector<int>> cost_matrix = getMatrix(n, m, "cost");
-    vector<int> supply = getVector(cost_matrix.size(), "supply", "origin ");
-    vector<int> demand = getVector(cost_matrix[0].size(), "demand", "destination ");
+    vector<vector<int>> cost_matrix = getMatrix(n, m, "COST");
+    vector<int> supply = getVector(cost_matrix.size(), "SUPPLY", "origin ");
+    vector<int> demand = getVector(cost_matrix[0].size(), "DEMAND", "destination ");
+
+    vector<vector<int>> res_matrix = north_west_method(cost_matrix, supply, demand);
 
     return 0;
 }
