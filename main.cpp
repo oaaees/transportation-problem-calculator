7c39699bbd394446511461b44a71c86be12c8369
Johan Paredes <jmpareds@gmail.com>
2023-09-29 22:29:22 -0400

    added function balance_cost_matrix  to balance the supply and demand

--- main.cpp
+++ main.cpp
@@ -24,7 +24,33 @@ vector<vector<int>> getMatrix(int n, int m, string name = ""){
     return data;
 }
 
-vector<vector<int>> north_west_method(vector<vector<int>> cost_matrix, vector<int> supply, vector<int> demand){
+void balance_cost_matrix(vector<vector<int>> &cost_matrix, vector<int> &supply, vector<int> &demand){
+    int sum_supply = 0, sum_demand = 0;
+
+    for(int x : supply){
+        sum_supply += x;
+    }
+
+    for(int x : demand){
+        sum_demand += x;
+    }
+
+    if(sum_supply == sum_demand) return;
+
+    if(sum_supply > sum_demand){
+        for (int i = 0; i < cost_matrix.size(); i++){
+            cost_matrix[i].push_back(0);
+        }
+        demand.push_back(sum_supply - sum_demand);
+    } else {
+        cost_matrix.push_back(vector<int>(cost_matrix[0].size(), 0));
+        supply.push_back(sum_demand - sum_supply);
+    }
+
+    return;
+}
+
+vector<vector<int>> north_west_method(vector<vector<int>> &cost_matrix, vector<int> supply, vector<int> demand){
     int n = cost_matrix.size(), m = cost_matrix[0].size();
     vector<vector<int>> res(n, vector<int>(m, 0));
 
@@ -35,14 +61,10 @@ vector<vector<int>> north_west_method(vector<vector<int>> cost_matrix, vector<in
             res[i][j] = supply[i];
             demand[j] -= supply[i]; 
             supply[i] = 0;
-        } else if (supply[i] > demand[j]) {
+        } else {
             res[i][j] = demand[j];
             supply[i] -= demand[j]; 
             demand[j] = 0;
-        } else {
-            res[i][j] = supply[i];
-            supply[i] = 0;
-            demand[j] = 0;   
         }
 
         if(supply[i]==0) i++;
@@ -50,6 +72,15 @@ vector<vector<int>> north_west_method(vector<vector<int>> cost_matrix, vector<in
     }
 
     return res;
+}
+
+void printMatrix(vector<vector<int>> m){
+    for(vector<int> x : m){
+        for (int y : x){
+            cout << y << " ";
+        }
+        cout << '\n';
+    }
 }
 
 int main (){
@@ -63,7 +94,10 @@ int main (){
     vector<int> supply = getVector(cost_matrix.size(), "SUPPLY", "origin ");
     vector<int> demand = getVector(cost_matrix[0].size(), "DEMAND", "destination ");
 
+    balance_cost_matrix(cost_matrix, supply, demand);
     vector<vector<int>> res_matrix = north_west_method(cost_matrix, supply, demand);
+
+    printMatrix(res_matrix);
 
     return 0;
 }
